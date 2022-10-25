5fd47866fe2f854aae2e37d268fac0630bbae352
andrewjwhite99 <116575311+andrewjwhite99@users.noreply.github.com>
2022-10-25 15:51:20 +0100

    Add files via upload

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,7 @@
+import os
 import requests
+import zipfile
+import io
 
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
@@ -8,12 +11,32 @@ download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip',
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip',
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip'
-]
+] 
+
+def unzip_to_folder(list_of_uris, target_directory):
+    for uri in list_of_uris:
+        req = requests.get(uri)
+        if req.ok:
+            zip = zipfile.ZipFile(io.BytesIO(req.content))
+            zip.extractall(target_directory)
+        else:
+            print("The file you are trying to access does not exist")
+
+
 
 
 def main():
-    # your code here
-    pass
+    # Create downloads directory
+    current_directory = os.getcwd()
+    directory_name = "downloads"
+    target_directory = os.path.join(current_directory, directory_name)
+
+    if not os.path.exists(target_directory):
+        os.mkdir(target_directory)
+
+    unzip_to_folder(download_uris, target_directory)
+
+    
 
 
 if __name__ == '__main__':
